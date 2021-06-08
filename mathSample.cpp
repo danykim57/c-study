14ff51c1c002de09a40ad22c294eb1503a76054f
danykim57 <55152550+danykim57@users.noreply.github.com>
2021-06-08 20:30:32 +0900

    Create mathSample.cpp

---
+++ mathSample.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+//c++ Math example
+
+int main() {
+	cout << max(1, 3) << endl;
+	cout << min(2, 4) << endl;
+	cout << sqrt(4) << endl;
+	cout << round(5.3) << endl;
+	cout << ceil(5.3) << endl;
+	cout << log(2) << endl;
+}
