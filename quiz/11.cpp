299e897c61925ee7ab134344d851c167d50928b1
danykim57 <55152550+danykim57@users.noreply.github.com>
2021-06-28 23:37:10 +0900

    Create 11.cpp

---
+++ quiz/11.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	//freopen("input.txt", "rt", stdin);
+	int n, i, cnt=0, tmp;
+	scanf("%d", &n);
+	for(i=1; i<=n; i++) {
+		tmp=i;
+		while(tmp>0){
+			tmp=tmp/10;
+			cnt++;
+		}
+	}
+	printf("%d\n", cnt);
+	return 0;
+}
