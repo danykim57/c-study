1affcd55e29ceb222d3c0b75d81de3a0ee744107
danykim57 <55152550+danykim57@users.noreply.github.com>
2021-06-30 16:19:12 +0900

    Create 22.cpp

---
+++ quiz/22.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <vector>
+int a[10], b[10];
+int main(int argc, char** argv){
+	//freopen("input.txt", "rt", stdin);
+	int n, k, i, sum=0, max;
+	scanf("%d %d", &n, &k);
+	std::vector<int> a(n);
+	for(i=0; i<n; i++){
+		scanf("%d", &a[i]);
+	}
+	for(i=0; i<k; i++){
+		sum+=a[i];
+	}
+	max=sum;
+	for(i=k; i<n; i++){
+		sum=sum+(a[i]-a[i-k]);
+		if(sum>max) max=sum;
+	}
+	printf("%d\n", max);
+	return 0;
+}
