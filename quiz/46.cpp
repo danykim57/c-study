a7c558e92c69795bc1911a141f026f1c8424e70c
danykim57 <55152550+danykim57@users.noreply.github.com>
2021-07-08 21:29:16 +0900

    Create 46.cpp

---
+++ quiz/46.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(int arc, char** argv){
+	//freopen("input.txt", "rt", stdin);
+	int n, k, i, p=0, cnt=0, tot=0;
+	scanf("%d", &n);
+	for(i=1; i<=n; i++){
+		scanf("%d", &a[i]);
+		tot+=a[i];
+	}
+	scanf("%d", &k);
+	if(k>=tot){
+		printf("-1\n");
+		return 0;
+	}
+	while(1){
+		p++;
+		if(p>n) p=1;
+		if(a[p]==0) continue;
+		a[p]--;
+		cnt++;
+		if(cnt==k) break;
+	}
+	while(1){
+		p++;
+		if(p>n) p=1;
+		if(a[p]!=0) break;
+	}
+	printf("%d\n", p);
+	
+	return 0;
+}
+	
