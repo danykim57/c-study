b64eaa3822e0f2d9b0ce15701cbacc06342f052f
danykim57 <55152550+danykim57@users.noreply.github.com>
2021-07-10 20:21:50 +0900

    Create 53.cpp

---
+++ quiz/53.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int stack[100], top=-1;
+
+void push(int x){
+	stack[++top]=x;
+}
+
+int pop(){
+	return stack[top--];
+}
+
+int main(int argc, char** argv){
+	//freopen("input.txt", "rt", stdin);
+    int n, k;
+    char str[20]="0123456789ABCDEF";
+    scanf("%d %d", &n, &k);
+    while(n>0){
+    	push(n%k);
+    	n=n/k;
+	}
+	while(top!=-1){
+		printf("%c", str[pop()]);
+	}
+	return 0;
+}
