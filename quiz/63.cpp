04be9e372ba1ad505bfcc213a244480a224d0e9d
danykim57 <55152550+danykim57@users.noreply.github.com>
2021-07-14 20:58:55 +0900

    Create 63.cpp

---
+++ quiz/63.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int map[51][51];
+int main() {
+	//freopen("input.txt", "rt", stdin);
+	int n, m, i, j, a, b, c;
+	scanf("%d %d", &n, &m);
+	for(i=1; i<=m; i++){
+		scanf("%d %d %d", &a, &b, &c);
+		map[a][b]=c;
+	}
+	for(i=1; i<=n; i++){
+		for(j=1; j<=n; j++){
+			printf("%d ", map[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
