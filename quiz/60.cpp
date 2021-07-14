4bd02a52fd55ac27760261acb11f85b9c72f1f97
danykim57 <55152550+danykim57@users.noreply.github.com>
2021-07-14 15:12:14 +0900

    Create 60.cpp

---
+++ quiz/60.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int n, a[11], total=0;
+bool flag=false;
+void DFS(int L, int sum){
+	if(sum>(total/2)) return;
+	if(flag==true) return;
+	if(L==n+1){
+		if(sum == (total-sum)){
+			flag=true;
+		}
+	}
+	else{
+		DFS(L+1, sum+a[L]);
+		DFS(L+1, sum);
+	}
+}
+
+int main(int argc, char** argv){
+	//freopen("input.txt", "rt", stdin);
+	scanf("%d", &n);
+	for(int i=0; i<n; i++){
+		scanf("%d", &a[i]);
+		total+=a[i];
+	}
+	DFS(1, 0);
+	if(flag) printf("YES");
+	else printf("NO");
+	
+	return 0;
+}
